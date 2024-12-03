14980b177f3ed41c461b5f0c878586cb78cf8748
malikharis1 <harismalik1@protonmail.com>
2024-12-04 00:17:09 +0530

    scan program added

---
+++ Scan.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Scan {
+   public static void main(String[] args) {
+
+      Scanner sc = new Scanner(System.in);
+      String var2 = sc.nextLine();
+      int var3 = sc.nextInt();
+      System.out.println(var2);
+      System.out.println(var3);
+      sc.close();
+   }
+
+}
