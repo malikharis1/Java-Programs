b482dc35d444e329b7e67c3e54f408bf2915bf0d
malikharis1 <harismalik1@protonmail.com>
2024-12-03 23:59:43 +0530

    first commit

---
+++ FIveNumberSUm.java
@@ -0,0 +1,14 @@
+package programs;
+
+public class FIveNumberSUm {
+    public static void main(String args[])
+    {
+        if(args.length ==5)
+        {
+
+        }
+        else{
+            System.out.println("incorrect values");
+        }
+    }
+}
---
+++ FiveString.java
@@ -0,0 +1,19 @@
+package programs;
+public class FiveString{
+    public static void main(String[] args)
+    {
+        if(args.length == 5)
+        {
+            String newvalue = "";
+            for(String arg : args)
+            {
+                newvalue+=arg;
+            }
+            System.out.println(newvalue);
+        }
+        else{
+            System.out.println("value is not correct");
+        }
+
+    }
+}
