4b042d43b331f4fd1eedcf6fcce533525a1e4d46
malikharis1 <harismalik1@protonmail.com>
2024-12-04 00:04:41 +0530

    two programs added

--- FIveNumberSUm.java
+++ FIveNumberSUm.java
@@ -5,6 +5,12 @@ public class FIveNumberSUm {
     {
         if(args.length ==5)
         {
+            int value =0;
+            for(String arg: args)
+            {
+                value+= Integer.parseInt(arg);
+            }
+            System.out.println(value);
 
         }
         else{
